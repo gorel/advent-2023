a93bac1d9ce6d3608ce88d242b8fa45b073112a7
Logan Gore <lgore93@gmail.com>
2023-12-02 14:40:48 -0800

    World's laziest template

---
+++ template.py
@@ -0,0 +1,11 @@
+# /usr/bin/env python3
+
+from advent.base import BaseSolver, Solution
+
+
+class Solver(BaseSolver):
+    def solve(self) -> Solution:
+        return Solution(None, None)
+
+
+Solver.run()
